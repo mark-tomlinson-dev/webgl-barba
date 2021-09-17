b72209f3c500656217f685c5d42ab635f72d089b
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-09-17 11:27:48 +1000

    create three js boilerplate

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-let camera, scene, renderer;
-let geometry, material, mesh;
+export default class Sketch {
+  constructor(options) {
+    this.container = options.domElement
+    this.width = this.container.offsetWidth
+    this.height = this.container.offsetHeight
 
-init();
+    this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
+    this.camera.position.z = 1;
+  
+    this.scene = new THREE.Scene();
 
-function init() {
+    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+    this.renderer.setSize( this.width, this.height );
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
-
-	scene = new THREE.Scene();
-
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
-
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
-
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
-  let container = document.getElementById('container')
-	container.appendChild( renderer.domElement );
-
+    this.container.appendChild(this.renderer.domElement)
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+  
+    this.time = 0;    
+    this.addObjects()
+    this.render()
+  }
+  
+  addObjects() {
+    this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    this.material = new THREE.MeshNormalMaterial();
+    
+    this.mesh = new THREE.Mesh( this.geometry, this.material );
+    this.scene.add( this.mesh );
+  }
+  
+  render() {
+    this.time += 0.05
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
+    
+    this.renderer.render( this.scene, this.camera );
+    requestAnimationFrame(this.render.bind(this))
+  }
 }
 
-function animation( time ) {
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
-
-}
+new Sketch({
+  domElement: document.getElementById('container')
+})
